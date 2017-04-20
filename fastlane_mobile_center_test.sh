0e90cb12c16120c7719eace17894cff4af976c86
Evgeniy Khramkov <ewgeniux@gmail.com>
2017-04-20 14:51:12 +0300

    fake xcode-select command

--- fastlane_mobile_center_test.sh
+++ fastlane_mobile_center_test.sh
@@ -2,5 +2,6 @@
 
 # this task runs unit tests for fastlane mobile_center_upload action
 
+cp ./xcode-select ../fastlane
 cd ../fastlane
 DEBUG=true bundle exec rspec ./fastlane/spec/actions_specs/mobile_center_upload_spec.rb
