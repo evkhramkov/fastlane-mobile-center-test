608fe947c16b1afa4877a83228cfeb40a9c72008
Evgeniy Khramkov <ewgeniux@gmail.com>
2017-04-20 15:11:58 +0300

    upd travis tasks

--- fastlane_mobile_center_test.sh
+++ fastlane_mobile_center_test.sh
@@ -2,7 +2,16 @@
 
 # this task runs unit tests for fastlane mobile_center_upload action
 
-cp ./xcode-select ../fastlane
+export PATH=$PATH:$PWD/fake-bin/
 cd ../fastlane
 bundle install
+
+echo ================================================================================
+echo ========================== RUNNING UNIT TESTS ==================================
+echo ================================================================================
+
 DEBUG=true bundle exec rspec ./fastlane/spec/actions_specs/mobile_center_upload_spec.rb
+
+echo ================================================================================
+echo ========================= USING FORKED FASTLANE ================================
+echo ================================================================================
